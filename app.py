66e57f28cfff4d79f67a608f6b2d025d0ceb90ca
yarungrung <s1243032@gm.ncue.edu.tw>
2025-10-29 03:57:36 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import streamlit as st
 # 注意：st.Page() 會自動尋找 .py 檔案
 pages = [
    st.Page("page_home.py", title="專案首頁", icon="🏠"),
-   st.Page("page_3Dmap-1.py", title="Pydeck 3D互動地圖瀏覽", icon="🌏"),
-   st.Page("page_3Dmap-2.py", title="Plotly 3D互動地圖瀏覽", icon="ℹ️")
+   st.Page("page_3dmap-1.py", title="Pydeck 3D互動地圖瀏覽", icon="🌏"),
+   st.Page("page_3dmap-2.py", title="Plotly 3D互動地圖瀏覽", icon="ℹ️")
 ]
 
 # 2. 使用 st.navigation() 建立導覽 (例如在側邊欄)
