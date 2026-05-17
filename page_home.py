95eb412b7cb35051df1546499d97b50615490e5c
yarungrung <s1243032@gm.ncue.edu.tw>
2026-05-17 03:50:23 +0000

    modified:   page_home.py

--- page_home.py
+++ page_home.py
@@ -6,11 +6,11 @@ st.write("這是一個使用 Streamlit 建立的3D互動式地圖應用程式。
 
 
 # 直接將 MP4 影片的 URL 傳給 st.video()
-video_url = "https://i.imgur.com/1GoAB0C.mp4"
+#video_url = "https://i.imgur.com/1GoAB0C.mp4"
 
-st.write(f"正在播放影片： {video_url}")
+#st.write(f"正在播放影片： {video_url}")
 
-st.video(video_url)
+#st.video(video_url)
 
 # 直接將 照片的 URL 傳給 st.image()
 image_url = "https://i.imgur.com/uf1T4ND.png"
