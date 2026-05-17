f73fba94fc5d45d86acdcca9fd54fb474cc8c3e8
yarungrung <s1243032@gm.ncue.edu.tw>
2026-05-17 03:57:24 +0000

    modified:   page_home.py

--- page_home.py
+++ page_home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 
 # 這裡放所有您想在首頁顯示的內容
 st.title("歡迎來到我的 3D GIS 專案！")
@@ -12,6 +13,10 @@ st.write("這是一個使用 Streamlit 建立的3D互動式地圖應用程式。
 
 #st.video(video_url)
 
+# 將上傳的照片叫出並呈現
+image = Image.open("41244.jpg")
+st.image(image, caption="圖片", use_container_width=True)
+
 # 直接將 照片的 URL 傳給 st.image()
-image_url = "https://i.imgur.com/uf1T4ND.png"
-st.image(image_url)
+#image_url = "https://i.imgur.com/uf1T4ND.png"
+#st.image(image_url)
